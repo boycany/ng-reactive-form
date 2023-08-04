e571187a920f76ddb7a028b21d637792b99c2dc5
Red Chien <redchien@viridiasolutions.com>
2023-08-04 16:41:11 +0800

    update

--- src/app/custom-validators.ts
+++ src/app/custom-validators.ts
@@ -1,4 +1,5 @@
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 export class CustomValidators {
   static invalidEmail(control: FormControl): { [s: string]: boolean } {
@@ -6,5 +7,20 @@ export class CustomValidators {
       return { invalidEmail: true };
     }
     return null;
+  }
+
+  static asyncInvalidEmail(
+    control: FormControl
+  ): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve) =>
+      setTimeout(
+        () =>
+          resolve(
+            control.value === 'XXXXXXXXXXXXX' ? { invalidEmail: true } : null
+          ),
+        1500
+      )
+    );
+    return promise;
   }
 }
